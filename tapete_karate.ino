9f941429109e6fe6f36c91dfe2ff9e60ced0bab4
Luis Acxis <luisacxis@gmail.com>
2020-08-25 02:10:53 -0500

    add setLevel by Serial.readString

--- tapete_karate.ino
+++ tapete_karate.ino
@@ -1,33 +1,41 @@
-/*---------------------------------------
-  Atributos
------------------------------------------*/
 int sensores[][3] = { // Array con los puertos del/los sensores
 //{triger, echo, led}
-  {53, 52, 51}, // Sensor 1
-  {50, 49, 48}, // Sensor 2
-  {47, 46, 45}, // Sensor 3
-  {44, 43, 42}, // Sensor 4
-  {41, 40, 39} // Sensor 5
+  {23, 22, 2}, // Sensor 1
+  {25, 24, 3}, // Sensor 2
+  {27, 26, 4}, // Sensor 3
+  {29, 28, 5}, // Sensor 4
+  {31, 30, 6}, // Sensor 5
+  {33, 32, 7}, // Sensor 6
+  {35, 34, 8}, // Sensor 7
+  {37, 36, 9}, // Sensor 8
+  {39, 38, 10} // Sensor 9
 };
-int distancia[] = {
-  0, // Distancia minima en cm
-  10 // Distancia maxima en cm
-};
-
 int triger = 0; // Establece la posicion triger como posición en el array
 int echo = 1; // Establece la posicion echo como posición en el array
 int led = 2; // Establece la posicion del/los leds en el array
 int sensores_length = (sizeof(sensores) / sizeof(int)) / 3;
 
+int niveles[] = { // Array con los sensores activos segun el nivel
+ 0, // Nivel 0  (Sensores activados: 0)
+ 6, // Nivel 1 (Sensores activados: 1 - 6)
+ 9, // Nivel 2 (Sensores activados: 1 - 9)
+ 12 // Nivel 3 (Sensores activados: 1 - 12)
+};
+
+int distancia[] = {
+  0, // Distancia minima en cm
+  10 // Distancia maxima en cm
+};
 int distanciaMin = 0; // Establece la posicion de la distancia minima como posición en el array
 int distanciaMax = 1; // Establece la posicion de la distancia maxima como posición en el array
 
+int nivel = -1;
 int sensor_activo = -1;
 
 /*---------------------------------------
   setup
 -----------------------------------------*/
-    
+   
 void setup() {
   Serial.begin(9600);
   randomSeed(analogRead(A0));
@@ -36,7 +44,9 @@ void setup() {
     pinMode(sensores[i][echo], INPUT); // Define echo como entrada
     pinMode(sensores[i][led], OUTPUT); // Define led como salida
   }
-  sensor_activo = getRandomSensor();
+  
+  setLevel(3); // Establece el nivel con el cual se iniciara el sketch
+  sensor_activo = getSensor();
 }
 
 /*---------------------------------------
@@ -44,9 +54,18 @@ void setup() {
 -----------------------------------------*/
 
 void loop() {
+  if(Serial.available()) {
+    String dataReceived = Serial.readString();
+    dataReceived.trim();
+    int dataNivel = dataReceived.toInt();
+    if(dataNivel >= 0 && dataNivel < (sizeof(niveles) / sizeof(int))) {
+      setLevel(dataNivel);
+    }
+  }
+  
   if(!enableSensor(sensor_activo)) {
     disableSensor(sensor_activo);
-    sensor_activo = getRandomSensor();
+    sensor_activo = getSensor();
   }
 }
 
@@ -58,13 +77,13 @@ void loop() {
  * @deprecated Obtiene un numero aleatorio entre 0 y el total de sensores -1
  * @return int Numero aleatorio
  */
-int getRandomSensor() {
+int getSensor() {
   int min = 0;
-  int max = sensores_length;
+  int max = niveles[nivel];
   while(true) {
-    int random_number = random(min, max);
-    if(random_number != sensor_activo || sensor_activo == -1) {
-      return random_number;
+    int random_sensor = random(min, max);
+    if(random_sensor != sensor_activo || sensor_activo == -1) {
+      return random_sensor;
     }
   }
 }
@@ -100,8 +119,18 @@ int getRandomSensor() {
   digitalWrite(sensores[sensor][triger], LOW);
   delayMicroseconds(4);
   digitalWrite(sensores[sensor][triger], HIGH); // Genera el pulso de triger
-  delayMicroseconds(10); 
+  delayMicroseconds(8);
   digitalWrite(sensores[sensor][triger], LOW);
   long tiempo = pulseIn(sensores[sensor][echo], HIGH);
   return tiempo * 0.0343 / 2; // Calcula la distancia obtenida en centimetros
+ }
+
+/**
+ * @deprecated Setea el nivel actual
+ * @param  int Especifica el nivel
+ */
+ void setLevel(int level) {
+  nivel = level;
+  disableSensor(sensor_activo);
+  sensor_activo = getSensor();
  }
